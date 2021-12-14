ac928398ccc449b88aee9e7f0021ea6df93fd218
yujong-lee <yujonglee@g.skku.edu>
2021-12-14 21:38:08 +0900

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,10 @@ module.exports = {
         ts: 'never',
       },
     ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
   },
   settings: {
     'import/resolver': {
