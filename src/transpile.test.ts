f9e13ed238cfa9c426beb221d0ef4259de102692
yujong-lee <yujonglee@g.skku.edu>
2021-12-17 11:29:07 +0900

    Add missing tests for better understanding

--- src/transpile.test.ts
+++ src/transpile.test.ts
@@ -3,55 +3,100 @@ import transpile from './transpile';
 
 import { removeWhiteSpaces } from './utils';
 
-const prismaSchema = /* Prisma */ `
-  enum Role {
-    USER
-    ADMIN
-  }
-
-  model Post {
-    authorId  Int?
-    content   Bytes?
-    id        Int     @default(autoincrement()) @id
-    published Boolean @default(false)
-    author    User?   @relation(fields: [authorId], references: [id])
-  }
-  
-  model User {
-    email String  @unique
-    id    Int     @default(autoincrement()) @id
-    name  String?
-    detail Bytes?
-    posts Post[]
-  }
-`;
-
-const graphqlSchema = `
-  scalar ByteArray
-
-  enum Role {
-    USER
-    ADMIN
-  }
-
-  type Post {
-    content: ByteArray
-    id: ID!
-    published: Boolean!
-    author: User
-  }
-
-  type User {
-    email: String!
-    id: ID!
-    name: String
-    detail: ByteArray
-    posts: [Post!]!
-  }
-`;
-
 describe('transpile', () => {
-  it('transpiles Prisma schema to graphql schema(SDL)', async () => {
+  it('adds scalars', async () => {
+    const prismaSchema = /* Prisma */ `
+      model Post {
+        id    Int      @id
+        date  DateTime
+        content1  Bytes?
+        content2  Bytes
+      }
+    `;
+
+    const graphqlSchema = `
+      scalar DateTime
+      scalar ByteArray
+
+      type Post {
+        id: ID!
+        date: DateTime!
+        content1: ByteArray
+        content2: ByteArray!
+      }
+    `;
+
+    const model = await parse(prismaSchema);
+
+    expect(removeWhiteSpaces(transpile(model)))
+      .toBe(removeWhiteSpaces(graphqlSchema));
+  });
+
+  it('adds enums', async () => {
+    const prismaSchema = /* Prisma */ `
+      enum Role {
+        USER
+        ADMIN
+      }
+      enum Gender {
+        male
+        female
+      }
+    `;
+
+    const graphqlSchema = `
+      enum Role {
+        USER
+        ADMIN
+      }
+      enum Gender {
+        male
+        female
+      }
+    `;
+
+    const model = await parse(prismaSchema);
+
+    expect(removeWhiteSpaces(transpile(model)))
+      .toBe(removeWhiteSpaces(graphqlSchema));
+  });
+
+  it('adds models', async () => {
+    const prismaSchema = /* Prisma */ `
+      model Post {
+        authorId  Int?
+        content   String?
+        id        Int     @default(autoincrement()) @id
+        published Boolean @default(false)
+        author    User?   @relation(fields: [authorId], references: [id])
+      }
+
+      model User {
+        email String  @unique
+        id    Int     @default(autoincrement()) @id
+        name  String?
+        detail String?
+        posts Post[]
+      }
+    `;
+
+    const graphqlSchema = `
+      type Post {
+        content: String
+        id: ID!
+        published: Boolean!
+        author: User
+      }
+
+      type User {
+        email: String!
+        id: ID!
+        name: String
+        detail: String
+        posts: [Post!]!
+      }
+    `;
+
     const model = await parse(prismaSchema);
 
     expect(removeWhiteSpaces(transpile(model)))
